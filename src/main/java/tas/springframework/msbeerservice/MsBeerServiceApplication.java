6d834b2198d90418ade5aa2201a513f73e5f8bc8
Totte Sjöman <totte.sjoman@gmail.com>
2020-05-11 07:46:36 +0300

    intial project setup

---
+++ src/main/java/tas/springframework/msbeerservice/MsBeerServiceApplication.java
@@ -0,0 +1,13 @@
+package tas.springframework.msbeerservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MsBeerServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MsBeerServiceApplication.class, args);
+	}
+
+}
---
+++ src/test/java/tas/springframework/msbeerservice/MsBeerServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package tas.springframework.msbeerservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MsBeerServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
